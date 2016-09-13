b82fb1134ba7bd9b8dad539cf20938781f7afa36
Nicklas Van Dam <Nicklas373@users.noreply.github.com>
2016-09-14 00:57:29 +0700

    Improve Impulse Governor
    cpufreq: impulse: don't skip waking up speedchange_task 
    cpufreq: impulse: Skip boost if already boosted
    cpufreq: impulse: Clean up freq selection 
    cpufreq: impulse: Add go_lowspeed_load
    cpufreq: impulse: tuning and burning values into governor

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -67,11 +67,16 @@ static spinlock_t speedchange_cpumask_lock;
 static struct mutex gov_lock;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
+#define HISPEED_FREQ 960000;
 static unsigned int hispeed_freq;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 99
+#define DEFAULT_GO_HISPEED_LOAD 95
 static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
+
+/*Go to lowest speed when CPU load at or below this value. */
+#define DEFAULT_GO_LOWSPEED_LOAD 10
+static unsigned long go_lowspeed_load = DEFAULT_GO_LOWSPEED_LOAD;
 
 /* Target load.  Lower values result in higher CPU speeds. */
 #define DEFAULT_TARGET_LOAD 90
@@ -83,13 +88,13 @@ static int ntarget_loads = ARRAY_SIZE(default_target_loads);
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
-#define DEFAULT_MIN_SAMPLE_TIME (80 * USEC_PER_MSEC)
+#define DEFAULT_MIN_SAMPLE_TIME (40 * USEC_PER_MSEC)
 static unsigned long min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 
 /*
  * The sample rate of the timer used to increase frequency
  */
-#define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
+#define DEFAULT_TIMER_RATE (30 * USEC_PER_MSEC)
 static unsigned long timer_rate = DEFAULT_TIMER_RATE;
 
 /*
@@ -114,7 +119,7 @@ static u64 boostpulse_endtime;
  * Max additional time to wait in idle, beyond timer_rate, at speeds above
  * minimum before wakeup to reduce speed, or -1 if unnecessary.
  */
-#define DEFAULT_TIMER_SLACK (4 * DEFAULT_TIMER_RATE)
+#define DEFAULT_TIMER_SLACK (1 * DEFAULT_TIMER_RATE)
 static int timer_slack_val = DEFAULT_TIMER_SLACK;
 
 /*
@@ -128,8 +133,10 @@ static bool align_windows = true;
  * Stay at max freq for at least max_freq_hysteresis before dropping
  * frequency.
  */
+#define DEFAULT_MAX_FREQ_HYSTERESIS 100000
 static unsigned int max_freq_hysteresis;
 
+#define DEFAULT_IO_IS_BUSY 1
 static bool io_is_busy;
 
 /* Round to starting jiffy of next evaluation window */
@@ -397,21 +404,20 @@ static void cpufreq_impulse_timer(unsigned long data)
 	do_div(cputime_speedadj, delta_time);
 	loadadjfreq = (unsigned int)cputime_speedadj * 100;
 	cpu_load = loadadjfreq / pcpu->policy->cur;
-	boosted = boost_val || now < boostpulse_endtime;
+	boosted = boost_val || now < boostpulse_endtime ||
+ 			cpu_load >= go_hispeed_load;
 	this_hispeed_freq = max(hispeed_freq, pcpu->policy->min);
 
-	if (cpu_load >= go_hispeed_load || boosted) {
-		if (pcpu->policy->cur < this_hispeed_freq) {
-			new_freq = this_hispeed_freq;
-		} else {
-			new_freq = choose_freq(pcpu, loadadjfreq);
-
+	if (cpu_load <= go_lowspeed_load && !boost_val) {
+ 		boosted = false;
+ 		new_freq = pcpu->policy->cpuinfo.min_freq;
+ 	} else {
+ 		new_freq = choose_freq(pcpu, loadadjfreq);
+ 	}
 			if (new_freq < this_hispeed_freq)
-				new_freq = this_hispeed_freq;
-		}
-	} else {
-		new_freq = choose_freq(pcpu, loadadjfreq);
-	}
+	
+	if (boosted)
+ 		new_freq = max(new_freq, this_hispeed_freq);
 
 	if (pcpu->policy->cur >= this_hispeed_freq &&
 	    new_freq > pcpu->policy->cur &&
@@ -463,7 +469,8 @@ static void cpufreq_impulse_timer(unsigned long data)
 		pcpu->floor_validate_time = now;
 	}
 
-	if (pcpu->target_freq == new_freq) {
+	if (pcpu->target_freq == new_freq &&
+			pcpu->target_freq <= pcpu->policy->cur) {
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto rearm_if_notmax;
 	}
@@ -627,9 +634,9 @@ static int cpufreq_impulse_speedchange_task(void *data)
 			}
 
 			if (max_freq != pcpu->policy->cur) {
-				__cpufreq_driver_target(pcpu->policy,
-							max_freq,
-							CPUFREQ_RELATION_H);
+			   __cpufreq_driver_target(pcpu->policy,
+ 							max_freq, 							CPUFREQ_RELATION_H);
+
 				for_each_cpu(j, pcpu->policy->cpus) {
 					pjcpu = &per_cpu(cpuinfo, j);
 					pjcpu->hispeed_validate_time = hvt;
@@ -945,6 +952,28 @@ static ssize_t store_go_hispeed_load(struct kobject *kobj,
 static struct global_attr go_hispeed_load_attr = __ATTR(go_hispeed_load, 0644,
 		show_go_hispeed_load, store_go_hispeed_load);
 
+static ssize_t show_go_lowspeed_load(struct kobject *kobj,
+ 				     struct attribute *attr, char *buf)
+ {
+ 	return sprintf(buf, "%lu\n", go_lowspeed_load);
+ }
+ 
+ static ssize_t store_go_lowspeed_load(struct kobject *kobj,
+ 			struct attribute *attr, const char *buf, size_t count)
+ {
+ 	int ret;
+ 	unsigned long val;
+ 
+ 	ret = strict_strtoul(buf, 0, &val);
+ 	if (ret < 0)
+ 		return ret;
+ 	go_lowspeed_load = val;
+ 	return count;
+ }
+ 
+ static struct global_attr go_lowspeed_load_attr = __ATTR(go_lowspeed_load, 0644,
+ 		show_go_lowspeed_load, store_go_lowspeed_load);
+ 
 static ssize_t show_min_sample_time(struct kobject *kobj,
 				struct attribute *attr, char *buf)
 {
@@ -1108,12 +1137,13 @@ static ssize_t store_io_is_busy(struct kobject *kobj,
 
 static struct global_attr io_is_busy_attr = __ATTR(io_is_busy, 0644,
 		show_io_is_busy, store_io_is_busy);
-
+ 
 static struct attribute *impulse_attributes[] = {
 	&target_loads_attr.attr,
 	&above_hispeed_delay_attr.attr,
 	&hispeed_freq_attr.attr,
 	&go_hispeed_load_attr.attr,
+	&go_lowspeed_load_attr.attr,
 	&min_sample_time_attr.attr,
 	&timer_rate_attr.attr,
 	&timer_slack.attr,
