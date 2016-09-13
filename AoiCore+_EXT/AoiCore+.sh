287946b2a2919468e51a3366d97d2ea2a0ad9e07
Nicklas Van Dam <herlambangdicky5@gmail.com>
2016-09-14 02:06:33 +0700

    Update AoiCore+ Component

--- AoiCore+_EXT/AoiCore+.sh
+++ AoiCore+_EXT/AoiCore+.sh
@@ -22,10 +22,13 @@ rm android/kernel/arch/arm/Kconfig
 cp AoiCore+_EXT/arch/arm/Kconfig android/kernel/arch/arm/Kconfig
 rm android/kernel/arch/arm/Makefile
 cp AoiCore+_EXT/arch/arm/Makefile android/kernel/arch/arm/Makefile
+rm android/kernel/arch/arm/boot/compressed/Makefile
+cp AoiCore+_EXT/arch/arm/boot/compressed/Makefile android/kernel/arch/arm/boot/compressed/Makefile
 rm android/kernel/arch/arm/configs/cyanogenmod_nicki_defconfig
 cp AoiCore+_EXT/arch/arm/configs/cyanogenmod_nicki_defconfig android/kernel/arch/arm/configs/cyanogenmod_nicki_defconfig
 rm android/kernel/arch/arm/include/asm/xor.h
 cp AoiCore+_EXT/arch/arm/include/asm/xor.h android/kernel/arch/arm/include/asm/xor.h
+cp AoiCore+_EXT/arch/arm/include/asm/rwsem.h android/kernel/arch/arm/include/asm/rwsem.h
 rm android/kernel/arch/arm/kernel/Makefile
 cp AoiCore+_EXT/arch/arm/kernel/Makefile android/kernel/arch/arm/kernel/Makefile
 cp AoiCore+_EXT/arch/arm/kernel/auto_hotplug.c android/kernel/arch/arm/kernel/auto_hotplug.c
@@ -40,6 +43,8 @@ rm android/kernel/arch/arm/vfp/vfpmodule.c
 cp AoiCore+_EXT/arch/arm/vfp/vfpmodule.c android/kernel/arch/arm/vfp/vfpmodule.c
 rm android/kernel/arch/arm/mach-msm/acpuclock-8627.c
 cp AoiCore+_EXT/arch/arm/mach-msm/acpuclock-8627.c android/kernel/arch/arm/mach-msm/acpuclock-8627.c
+rm android/kernel/arch/arm/mach-msm/dma_test.c
+cp AoiCore+_EXT/arch/arm/mach-msm/dma_test.c android/kernel/arch/arm/mach-msm/dma_test.c
 cp AoiCore+_EXT/arch/arm/mach-msm/fastchg.c android/kernel/arch/arm/mach-msm/fastchg.c
 rm android/kernel/arch/arm/mach-msm/Kconfig
 cp AoiCore+_EXT/arch/arm/mach-msm/Kconfig android/kernel/arch/arm/mach-msm/Kconfig
@@ -62,34 +67,40 @@ cp AoiCore+_EXT/drivers/cpufreq/Makefile android/kernel/drivers/cpufreq/Makefile
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_HYPER.c android/kernel/drivers/cpufreq/cpufreq_HYPER.c
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_intellidemand.c android/kernel/drivers/cpufreq/cpufreq_intellidemand.c
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_intellimm.c android/kernel/drivers/cpufreq/cpufreq_intellimm.c
+cp AoiCore+_EXT/drivers/cpufreq/cpufreq_impulse.c android/kernel/drivers/cpufreq/cpufreq_impulse.c
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_lionheart.c android/kernel/drivers/cpufreq/cpufreq_lionheart.c
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_lulzactive.c android/kernel/drivers/cpufreq/cpufreq_lulzactive.c
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_smartass2.c android/kernel/drivers/cpufreq/cpufreq_smartass2.c
 cp AoiCore+_EXT/drivers/cpufreq/cpufreq_savagedzen.c android/kernel/drivers/cpufreq/cpufreq_savagedzen.c
+rm android/kernel/drivers/crypto/msm/qcedev.c
+cp AoiCore+_EXT/drivers/crypto/msm/qcedev.c android/kernel/drivers/crypto/msm/qcedev.c
+rm android/kernel/drivers/thermal/Kconfig
+cp AoiCore+_EXT/drivers/thermal/Kconfig android/kernel/drivers/thermal/Kconfig
 rm android/kernel/drivers/usb/otg/msm_otg.c
 cp AoiCore+_EXT/drivers/usb/otg/msm_otg.c android/kernel/drivers/usb/otg/msm_otg.c
+rm android/kernel/fs/compat_ioctl.c
+cp AoiCore+_EXT/fs/compat_ioctl.c android/kernel/fs/compat_ioctl.c
+rm android/kernel/include/asm-generic/ioctls.h
+cp AoiCore+_EXT/include/asm-generic/ioctls.h android/kernel/include/asm-generic/ioctls.h
 cp AoiCore+_EXT/include/linux/compiler-gcc5.h android/kernel/include/linux/compiler-gcc5.h
 rm android/kernel/include/linux/cpufreq.h
 cp AoiCore+_EXT/include/linux/cpufreq.h android/kernel/include/linux/cpufreq.h
 cp AoiCore+_EXT/include/linux/fastchg.h android/kernel/include/linux/fastchg.h
+cp AoiCore+_EXT/include/linux/quickwakeup.h android/kernel/include/linux/quickwakeup.h
 rm android/kernel/kernel/sched/features.h
 cp AoiCore+_EXT/kernel/sched/features.h android/kernel/kernel/sched/features.h
 cd android/kernel
-
+rm android/kernel/power/Kconfig
+cp AoiCore+_EXT/kernel/power/Kconfig android/kernel/power/Kconfig
+rm android/kernel/power/Makefile
+cp AoiCore+_EXT/kernel/power/Makefile android/kernel/power/Makefile
+rm android/kernel/power/quickwakeup.c
+cp AoiCore+_EXT/kernel/power/quickwakeup.c android/kernel/power/quickwakeup.c
 echo "
-###Running GCC Toolchains 5.2.0 (ArchiToolchains)"
+###Running GCC Toolchains 5.3.0 (Crosstool-NG Toolchains)"
 
-export CT_ARCH_ARCH=""
-export CT_ARCH_CPU="cortex-a7"
-export CT_ARCH_TUNE="cortex-a7"
-export CT_ARCH_FPU="neon-vfpv4"
-export CT_ARCH_FLOAT_HW=y
-export CT_ARCH_FLOAT="hard"
-export CT_ARCH_SUPPORT_SOFTFP=y
-export CT_ARCH_ARM_MODE="arm"
-export CT_ARCH_ARM_MODE_ARM=y
 export ARCH=arm
-export CROSS_COMPILE=/home/nicklas/toolchainsarchi/bin/arm-architoolchain-linux-gnueabihf-
+export CROSS_COMPILE=/home/nicklas/crosstool-toolchains/bin/arm-unknown-linux-gnueabihf-
 
 echo "
 ###Building zImage"
@@ -98,7 +109,7 @@ echo "
 ###Compiler process is written on compileLog for simple interfaces"
 
 make ARCH=arm cyanogenmod_nicki_defconfig
-make ARCH=arm CROSS_COMPILE=/home/nicklas/toolchainsarchi/bin/arm-architoolchain-linux-gnueabihf- > compileLog
+make ARCH=arm CROSS_COMPILE=/home/nicklas/crosstool-toolchains/bin/arm-unknown-linux-gnueabihf- > compileLog
 
 echo "
 ##Creating Modules kernel"
@@ -118,29 +129,9 @@ echo "##Cleaning Build"
 cd android/kernel
 make clean && make mrproper
 
-echo "## Creating boot.img"
-cd /home/nicklas/AoiCore+_EXT/AIK-Linux
-./unpackimg.sh
-cd /home/nicklas/AoiCore+_output
-rm /home/nicklas/AoiCore+_EXT/AIK-Linux/split_img/boot.img-zImage
-cp zImage /home/nicklas/AoiCore+_EXT/AIK-Linux/split_img/boot.img-zImage
-cd /home/nicklas/AoiCore+_EXT/AIK-Linux
-./repackimg.sh
-cd /home/nicklas/AoiCore+_EXT/AIK-Linux/
-mv image-new.img /home/nicklas/boot.img
-
-echo "##Cleaning old boot.img"
-cd /home/nicklas/AoiCore+_EXT/AIK-Linux
-./cleanup.sh
-
-echo "##Done"
-
 echo "##Creating AoiCore+.zip"
-rm /home/nicklas/AoiCore+_output/zImage
 cd /home/nicklas/AoiCore+_EXT
 ./AoiCore+_Builder.sh
-##Script Done
 
 echo "Script Complete Successfuly"
 echo "Nicklas Van Dam @ XDA"
-
--- AoiCore+_EXT/AoiCore+_Builder.sh
+++ AoiCore+_EXT/AoiCore+_Builder.sh
@@ -2,12 +2,14 @@
 echo "AoiCore+.zip Builder"
 echo "#######Nicklas Van Dam#######"
 echo "###Starting...###"
+cd /home/nicklas/AoiCore+_EXT
 mkdir work
 cp Aoi_Core+.zip work
 cd work
 unzip Aoi_Core+.zip
-cd /home/nicklas
-mv boot.img AoiCore+_EXT/work
+rm tmp/kernel/boot.img-zImage
+cd /home/nicklas/AoiCore+_output
+mv zImage /home/nicklas/AoiCore+_EXT/work/tmp/kernel/boot.img-zImage
 cd /home/nicklas/AoiCore+_EXT/work/system/lib
 mkdir modules
 cd /home/nicklas/AoiCore+_output
@@ -15,11 +17,14 @@ mv *.ko /home/nicklas/AoiCore+_EXT/work/system/lib/modules
 cd /home/nicklas/AoiCore+_EXT/work
 rm Aoi_Core+.zip
 echo "###Creating AoiCore+.zip###"
-zip -r Aoi_Core+_v1.4.3-nicki.zip *
+zip -r Aoi_Core+_v1.5.5-nicki *
 rm -rfv META-INF
 rm -rfv system 
+rm -rfv tmp
+mv Aoi_Core+_v1.5.5-nicki /home/nicklas/Aoi_Core+_v1.5.6-nicki.zip
+echo "###Cleaning..."
 cd /home/nicklas/AoiCore+_EXT
 rm -rfv work
-mv Aoi_Core+_v1.4.3-nicki.zip /home/nicklas
-cd /home/nicklas/AoiCore+_EXT
-echo "###Done..." 
+cd /home/nicklas
+rm -rvf AoiCore+_output
+rm -rvf AoiCore+_output
